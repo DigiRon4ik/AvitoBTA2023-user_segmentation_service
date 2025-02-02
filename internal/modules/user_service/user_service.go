217f65de2aa209c4eacce92945e7d27a1dd75839
DigiRon4ik <dr.digiron@gmail.com>
2025-02-02 21:09:50 +0300

    [feature] minimal user_service.go

---
+++ internal/modules/user_service/user_service.go
@@ -0,0 +1,49 @@
+package user_service
+
+import (
+	"context"
+
+	"user_segmentation_service/internal/models"
+)
+
+type DB interface {
+	CreateUser(ctx context.Context, user *models.User) error
+	DeleteUser(ctx context.Context, userID int) error
+	UpdateUser(ctx context.Context, user *models.User) error
+	GetUserByID(ctx context.Context, userID int) (*models.User, error)
+	GetAllUsers(ctx context.Context) ([]*models.User, error)
+}
+
+type UserService struct {
+	store DB
+}
+
+func NewUserService(store DB) *UserService {
+	return &UserService{store: store}
+}
+
+func (s *UserService) Create(ctx context.Context, user *models.User) (*models.User, error) {
+	if err := s.store.CreateUser(ctx, user); err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (s *UserService) Delete(ctx context.Context, userID int) error {
+	return s.store.DeleteUser(ctx, userID)
+}
+
+func (s *UserService) Update(ctx context.Context, user *models.User) (*models.User, error) {
+	if err := s.store.UpdateUser(ctx, user); err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (s *UserService) GetByID(ctx context.Context, userID int) (*models.User, error) {
+	return s.store.GetUserByID(ctx, userID)
+}
+
+func (s *UserService) GetAll(ctx context.Context) ([]*models.User, error) {
+	return s.store.GetAllUsers(ctx)
+}
