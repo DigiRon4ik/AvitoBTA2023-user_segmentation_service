734987c20bc4c543b3846065a4709627e0b87a5d
DigiRon4ik <dr.digiron@gmail.com>
2025-02-02 21:37:58 +0300

    [feature] minimal handlers/segment.go without logic

--- internal/server/handlers/segment.go
+++ internal/server/handlers/segment.go
@@ -1,1 +1,46 @@
 package handlers
+
+import (
+	"context"
+	"net/http"
+
+	"user_segmentation_service/internal/models"
+)
+
+type segmentService interface {
+	Create(ctx context.Context, seg *models.Segment) (*models.Segment, error)
+	Delete(ctx context.Context, slug string) error
+	Update(ctx context.Context, seg *models.Segment) (*models.Segment, error)
+	GetByID(ctx context.Context, slug string) (*models.Segment, error)
+	GetAll(ctx context.Context) ([]*models.Segment, error)
+}
+
+type SegmentHandlers struct {
+	segments segmentService
+}
+
+func NewSegmentHandlers(ss segmentService) *SegmentHandlers {
+	return &SegmentHandlers{
+		segments: ss,
+	}
+}
+
+func (uh *SegmentHandlers) CreateHandler(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+func (uh *SegmentHandlers) DeleteHandler(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+func (uh *SegmentHandlers) UpdateHandler(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+func (uh *SegmentHandlers) GetHandler(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+func (uh *SegmentHandlers) GetAllHandler(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
