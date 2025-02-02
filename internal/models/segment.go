bd37e127877c7421ae293ebbd2bd126a7eab5503
DigiRon4ik <dr.digiron@gmail.com>
2025-02-02 23:34:25 +0300

    [fix] add omitempty all field

--- internal/models/segment.go
+++ internal/models/segment.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type Segment struct {
-	ID          int       `json:"id" db:"id"`
-	Slug        string    `json:"slug" db:"slug"`
-	Description string    `json:"description" db:"description"`
-	CreatedAt   time.Time `json:"created_at" db:"created_at"`
+	ID          int       `json:"id,omitempty" db:"id"`
+	Slug        string    `json:"slug,omitempty" db:"slug"`
+	Description string    `json:"description,omitempty" db:"description"`
+	CreatedAt   time.Time `json:"created_at,omitempty" db:"created_at"`
 }
