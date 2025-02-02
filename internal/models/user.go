54209e6292d01d1fe89aca39cae4cf522471840f
DigiRon4ik <dr.digiron@gmail.com>
2025-02-02 22:57:27 +0300

    [fix] add omitempty all field

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type User struct {
-	ID        int       `json:"id" db:"id"`
-	Name      string    `json:"name" db:"name"`
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	ID        int       `json:"id,omitempty" db:"id"`
+	Name      string    `json:"name,omitempty" db:"name"`
+	CreatedAt time.Time `json:"created_at,omitempty" db:"created_at"`
 }
