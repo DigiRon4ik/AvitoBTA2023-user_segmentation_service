68b3d33ff7886561c8d4d91471138ebaae884179
DigiRon4ik <dr.digiron@gmail.com>
2025-02-02 21:42:42 +0300

    [feature] add routes for the segments

--- internal/server/router.go
+++ internal/server/router.go
@@ -2,11 +2,18 @@ package server
 
 import "user_segmentation_service/internal/server/handlers"
 
-func (s *APIServer) configureRouter() {
-	userHandlers := handlers.NewUserHandlers(s.us)
-	s.router.HandleFunc("POST /users", userHandlers.CreateHandler)
-	s.router.HandleFunc("DELETE /users/{id}", userHandlers.DeleteHandler)
-	s.router.HandleFunc("PUT /users/{id}", userHandlers.UpdateHandler)
-	s.router.HandleFunc("GET /users/{id}", userHandlers.GetHandler)
-	s.router.HandleFunc("GET /users", userHandlers.GetAllHandler)
+func (api *APIServer) configureRouter() {
+	userHandlers := handlers.NewUserHandlers(api.us)
+	api.router.HandleFunc("POST /users", userHandlers.CreateHandler)
+	api.router.HandleFunc("DELETE /users/{id}", userHandlers.DeleteHandler)
+	api.router.HandleFunc("PUT /users/{id}", userHandlers.UpdateHandler)
+	api.router.HandleFunc("GET /users/{id}", userHandlers.GetHandler)
+	api.router.HandleFunc("GET /users", userHandlers.GetAllHandler)
+
+	segmentHandlers := handlers.NewSegmentHandlers(api.ss)
+	api.router.HandleFunc("POST /segments", segmentHandlers.CreateHandler)
+	api.router.HandleFunc("DELETE /segments/{slug}", segmentHandlers.DeleteHandler)
+	api.router.HandleFunc("PUT /segments/{slug}", segmentHandlers.UpdateHandler)
+	api.router.HandleFunc("GET /segments/{slug}", segmentHandlers.GetHandler)
+	api.router.HandleFunc("GET /segments", segmentHandlers.GetAllHandler)
 }
