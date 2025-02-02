3ed54c7427536aed96bf63bb9bf237e34c9d29dd
DigiRon4ik <dr.digiron@gmail.com>
2025-02-02 21:11:17 +0300

    [feature] minimal handlers/user.go without logic

--- internal/server/handlers/user.go
+++ internal/server/handlers/user.go
@@ -1,1 +1,46 @@
 package handlers
+
+import (
+	"context"
+	"net/http"
+
+	"user_segmentation_service/internal/models"
+)
+
+type userService interface {
+	Create(ctx context.Context, user *models.User) (*models.User, error)
+	Delete(ctx context.Context, userID int) error
+	Update(ctx context.Context, user *models.User) (*models.User, error)
+	GetByID(ctx context.Context, userID int) (*models.User, error)
+	GetAll(ctx context.Context) ([]*models.User, error)
+}
+
+type UserHandlers struct {
+	users userService
+}
+
+func NewUserHandlers(us userService) *UserHandlers {
+	return &UserHandlers{
+		users: us,
+	}
+}
+
+func (uh *UserHandlers) CreateHandler(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+func (uh *UserHandlers) DeleteHandler(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+func (uh *UserHandlers) UpdateHandler(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+func (uh *UserHandlers) GetHandler(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+func (uh *UserHandlers) GetAllHandler(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
